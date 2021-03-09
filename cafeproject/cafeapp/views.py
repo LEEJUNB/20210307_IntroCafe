3a136594a031e5530d0b049631bcefa7f31e73d3
LEEJUNB <topguide82@gmail.com>
2021-03-09 23:24:34 +0900

    각 이미지 파일에 해당되는 html파일

--- cafeproject/cafeapp/views.py
+++ cafeproject/cafeapp/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def home(request) : 
     return render(request,'index.html')
-
 def detail(request) : 
+    return render(request,'portfolio-details.html')
+def detail1(request) : 
+    return render(request,'portfolio-details1.html')
+def detail2(request) : 
+    return render(request,'portfolio-details2.html')
+def detail3(request) : 
+    return render(request,'portfolio-details3.html')
+def detail4(request) : 
+    return render(request,'portfolio-details.html')
+def detail5(request) : 
+    return render(request,'portfolio-details.html')
+def detail6(request) : 
+    return render(request,'portfolio-details.html')
+def detail7(request) : 
+    return render(request,'portfolio-details.html')
+def detail8(request) : 
+    return render(request,'portfolio-details.html')
+def detail9(request) : 
     return render(request,'portfolio-details.html')
--- cafeproject/cafeproject/urls.py
+++ cafeproject/cafeproject/urls.py
@@ -5,5 +5,14 @@ from cafeapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('detail/', views.detail, name="detail")
+    path('detai/', views.detail, name="detail"),
+    path('detail/', views.detail1, name="detail1"),
+    path('detai2/', views.detail2, name="detail2"),
+    path('detai3/', views.detail3, name="detail3"),
+    path('detai4/', views.detail4, name="detail4"),
+    path('detai5/', views.detail5, name="detail5"),
+    path('detai6/', views.detail6, name="detail6"),
+    path('detai7/', views.detail7, name="detail7"),
+    path('detai8/', views.detail8, name="detail8"),
+    path('detai9/', views.detail9, name="detail9"),
 ]
